02c5231033ded82b3fd7a2cfef89888963ce71e0
Isaac Dearman <isaac.dearman@gmail.com>
2024-12-23 10:38:40 +0000

    wire up external js files to script.js

--- assets/location_script.js
+++ assets/location_script.js
@@ -1,6 +1,17 @@
+/**
+* This function is called by pressing the get my location button.
+* It will insert the user's location into the location input field.
+*/
 function insertLocation() {
+
     let locationInput = document.querySelector('.locationInput');
+
     navigator.geolocation.getCurrentPosition(function(position) {
         locationInput.value = position.coords.latitude + ',' + position.coords.longitude;
+        console.log(locationInput.value);
     });
+
+    console.log();
 }
+
+export {insertLocation};
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,21 @@
+import {insertLocation} from './location_script.js';
 
+console.log('Hello, world!');
 /**
  * This generates the event listeners for the main page of the website.
  */
 document.addEventListener('DOMContentLoaded', function() {
     let locationButton = document.querySelector('.locationButton');
 
-    locationButton.addEventListener('click', function() {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            console.log(position);
-            console.log(position.coords.latitude);
-            console.log(position.coords.longitude);
+    locationButton.addEventListener('click', function(x) {
+        console.log('Geolocation is available');
+        navigator.geolocation.getCurrentPosition(function() {
+            
+            insertLocation()
+
+            // console.log(position);
+            // console.log(position.coords.latitude);
+            // console.log(position.coords.longitude);
         });
     });
 
