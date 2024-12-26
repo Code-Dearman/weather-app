49ef2b04d11fdf85174c79889a4135e903844118
Isaac Dearman <isaac.dearman@gmail.com>
2024-12-26 10:53:40 +0000

    add: event listner for next button

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 ## Credits / References
 
 - [MDN](https://developer.mozilla.org/en-US/) for docs relating to all parts of the project. 
+- [Google Geocoding API docs](https://developers.google.com/maps/documentation/geocoding/overview)
--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,36 @@ import {insertLocation} from './location_script.js';
  * This generates the event listeners for the main page of the website.
  */
 document.addEventListener('DOMContentLoaded', function() {
-    let locationButton = document.querySelector('.locationButton');
 
+    /**
+     * Event listnener for button which calls the function the get the user's
+     * current location.
+     */
+    let locationButton = document.querySelector('.locationButton');
     locationButton.addEventListener('click', function(event) {
 
         event.preventDefault();
         insertLocation();
     });
+
+    /**
+     * Event listener for the next button on the main page.
+     */
+    let nextButton = document.querySelectorAll('.nextButton');
+    nextButton.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+
+            event.preventDefault();
+            let current = document.querySelector('.current');
+            let next = current.nextElementSibling;
+            current.classList.remove('current');
+            next.classList.add('current');
+        });
+    });
+
+    /**
+     * Event listener for the previous button on the main page.
+     */
+
 
 });
